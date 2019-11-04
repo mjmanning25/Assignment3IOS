3a6a4c16c98fb18a08f2c231bed5b671b4343250
Michael <mjmanning25@gmail.com>
2019-11-04 13:44:56 +1300

    test

---
+++ assignment3/assignment3/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  assignment3
+//
+//  Created by Onisha Juhl on 4/11/19.
+//  Copyright © 2019 Michael Manning. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
